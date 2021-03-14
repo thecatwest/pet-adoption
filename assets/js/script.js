ffb33b30d08de638411a2b75774d85424caf748f
TheCatWest <catwestover@outlook.com>
2021-03-13 20:10:09 -0700

    fetch api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,21 @@
-// Make form submit handler for pet search
-//     if else statement
-// var getPetProfiles
-//     fetch apiUrlPet
-//     if else statement
-//     catch any errors with .catch(function(error) {alert})
-
-// var displayProfiles = function (profiles, searchTerms)
-//     if statement with return to check if api returned any profiles
-
-//     profileSearchTerms.textContent = searchTerms;
-//     loop over profiles
-//     for statement
-//         format repo name
-//         var profileName = profiles[i].pet.name + "/" + profiles[i].name
-
-//         create span element to hold pet name
-//         var petNameEl = document.createElement("span");
-//         petNameEl.textContent =
-
-fetch("https://test1-api.rescuegroups.org/v5/2p3gpOLU")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+fetch("https://test1-api.rescuegroups.org/v5/public/animals/breeds/526", {
+  headers: {
+    "Content-Type": "application/vnd.api+json",
+    Authorization: "2p3gpOLU",
+  },
+}).then(function (response) {
+  response.text().then((data) => {
+    console.log(JSON.parse(data));
   });
+});
 
-var getPetProfiles = function() {
-    console.log("function was called");
-};
-
-getPetProfiles();
+fetch("https://test1-api.rescuegroups.org/v5/public/animals/species/24", {
+  headers: {
+    "Content-Type": "application/vnd.api+json",
+    Authorization: "2p3gpOLU",
+  },
+}).then(function (response) {
+  response.text().then((data) => {
+    console.log(JSON.parse(data));
+  });
+});
